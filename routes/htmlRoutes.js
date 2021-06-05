5b917d36f6b5f71282669c1508ae077004e58ea0
Jamie Kaczor <dignanjk@aol.com>
2021-06-05 19:42:16 -0400

    updates

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const { builtinModules } = require('module');
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// All other routes will respond with the index.html file
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+modules.exports = router;
