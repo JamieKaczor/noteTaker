8992fe525a7c9b57711d50d0e813c0b7ae67ec00
Jamie Kaczor <dignanjk@aol.com>
2021-06-11 18:50:07 -0400

    figuring stuff out

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,6 @@
 const router = require('express').Router();
-const store = require('../db/store');
+const notesRoutes = require('./notesRoutes');
 
-// create a route that responds with all notes coming from the database
-
-router.get('/notes', (req, res) => {
-    store.getNotes()
-    .then((notes) => {
-        return res.json(notes)
-    })
-    // .catch((err) => res.status(500).json(err))
-})
-
-localhost:/3000/api/notes
-
-
-
-
-
+router.use(notesRoutes);
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,12 @@
-const { builtinModules } = require('module');
 const path = require('path');
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
-// All other routes will respond with the index.html file
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
-
-modules.exports = router;
+module.exports = router;
